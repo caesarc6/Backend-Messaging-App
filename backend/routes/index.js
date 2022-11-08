b51501f15d3b18c6fb370d927fa636afdb7324f2
ramee <ramee.yudeh@gmail.com>
2022-11-08 18:27:10 -0500

    changes

---
+++ backend/routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+module.exports = router;
+
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'http://api.weatherapi.com/v1/current.json',
+  params: {q: 'London', dt: '2022-12-25', key: '38556edbb21c4d2ea89231336221810'},
+  headers: {
+    'X-RapidAPI-Key': '38556edbb21c4d2ea89231336221810',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+const accountSid = ' '; // Your Account SID from www.twilio.com/console
+const authToken = ' '; // Your Auth Token from www.twilio.com/console
+const twilio = require('twilio');
+
+const client = new twilio(accountSid, authToken);
+
+client.messages
+  .create({
+    body: 'Hello from Node',
+    to: '+', // Text this number
+    from: '+', // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  axios.request(options).then(function (response) {
+    res.render('weather',  {response:response.data}  );
+  }).catch(function (error) {
+    console.error(error);
+  });
+  
+});
