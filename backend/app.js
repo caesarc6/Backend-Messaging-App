4f40dc9c161a7952002ca691235d2660db8311a0
Damian <damianpreslyperera@gmail.com>
2022-11-15 18:14:31 -0500

    Added send route and updated app.js

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sendRouter = require('./routes/send');
 
 var app = express();
+
+app.get('/',(req, res) => {
+  res.send('Hello World!, This is from file app.js')
+})
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/send', sendRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
