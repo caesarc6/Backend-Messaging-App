79e0c50da57d531b9841cd6283d39cd1dcd310f6
ramee <ramee.yudeh@gmail.com>
2022-11-15 19:28:22 -0500

    patch

--- backend/routes/send.js
+++ backend/routes/send.js
@@ -13,23 +13,23 @@ require('dotenv').config();
 
 
 router.post('/', (req, res) => {
-    //console.log('POST request:', req.body);
+    console.log('POST request:', req.body);
     console.log('Name:',req.body.name);
     console.log('Phone number:',req.body.phone);
     console.log('Text message:',req.body.message);
-    res.send('Got a POST request, test route')
+    res.send('Got a POST request')
 
     var text_message = req.body.message;
     var phone_number = req.body.phone;
 
     const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-    const authToken = process.env.AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+    const authToken = process.env.AUTHENTICATION_TOKEN; // Your Auth Token from www.twilio.com/console
 
 
     const client = new twilio(accountSid, authToken);
 
-const my_number = process.env.MY_PHONE_NUMBER;
-const twilio_number = process.env.TWILIO_PHONE_NUMBER;
+const my_number = process.env.PHONE_NUMBER;
+const twilio_number = process.env.TWILIO_PHONENUM;
 
 client.messages
   .create({
