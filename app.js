72c3a6a48af49fa11f9922bbe0e3309281c1c3e4
Damian <damianpreslyperera@gmail.com>
2022-11-29 20:24:48 -0500

    Functions firebase

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var functions = require('firebase-functions');
 // import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
