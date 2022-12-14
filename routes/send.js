3562f5bcbfe03669dd50d298127ec3a359481c13
Damian <damianpreslyperera@gmail.com>
2022-12-13 20:23:37 -0500

    app

--- routes/send.js
+++ routes/send.js
@@ -11,42 +11,42 @@ const twilio = require('twilio');
 require('dotenv').config();
 
 var cors = require('cors');
-router.options('/',cors());
+router.options('/', cors());
 
 
 
 router.post('/', (req, res) => {
-    //console.log('POST request:', req.body);
-    console.log(req.headers);
-    if (req.headers['x-post-380'] !== 'sonyl') return;
+  //console.log('POST request:', req.body);
+  console.log(req.headers);
+  if (req.headers['x-post-380'] !== 'sonyl') return;
 
-    console.log('Name:',req.body.name);
-    console.log('Phone number:',req.body.phone);
-    console.log('Text message:',req.body.message);
-    res.send('Got a POST request, message sent')
-
-    var text_message = req.body.message;
-    var phone_number = req.body.phone;
-
-    const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-    const authToken = process.env.AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+  console.log('Name:', req.body.name);
+  console.log('Phone number:', req.body.phone);
+  console.log('Text message:', req.body.message);
 
 
-    const client = new twilio(accountSid, authToken);
+  var text_message = req.body.message;
+  var phone_number = req.body.phone;
 
-//const my_number = process.env.MY_PHONE_NUMBER;
-const twilio_number = process.env.TWILIO_PHONE_NUMBER;
+  const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+  const authToken = process.env.AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
-client.messages
-  .create({
-    body: text_message,
-    to: phone_number, // Text this number
-    from: twilio_number , // From a valid Twilio number
-  })
-  .then((message) => console.log(message.sid));
 
-  })
+  const client = new twilio(accountSid, authToken);
 
- 
+  //const my_number = process.env.MY_PHONE_NUMBER;
+  const twilio_number = process.env.TWILIO_PHONE_NUMBER;
 
-  
+  client.messages
+    .create({
+      body: text_message,
+      to: phone_number, // Text this number
+      from: twilio_number, // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+    res.send(`${req.body.name}, ${req.body.phone}, ${req.body.message}`);
+
+})
+
+
+
